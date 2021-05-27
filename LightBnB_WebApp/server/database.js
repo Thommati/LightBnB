5e6662ef8b14f73c4a78e21fd0ae8496491741b8
Matthew Thompson <supra__charged@hotmail.com>
2021-05-27 13:26:04 -0600

    getAllProperties works with filter object

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -88,9 +88,69 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
+  const queryParams = [];
+  let queryString = `
+  SELECT properties.*, AVG(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+  
+  let whereAdded = false;
+  
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += ` WHERE city LIKE $${queryParams.length}`;
+    whereAdded = true;
+  }
+  
+  if (options.owner_id) {
+    queryParams.push(`${options.owner_id}`);
+    if (whereAdded) {
+      queryString += ' AND ';
+    } else {
+      queryString += ' WHERE ';
+      whereAdded = true;
+    }
+    queryString += `owner_id = $${queryParams.length}`;
+  }
+  
+  if (options.minimum_price_per_night) {
+    queryParams.push(`${options.minimum_price_per_night}`);
+    if (whereAdded) {
+      queryString += ' AND ';
+    } else {
+      queryString += ' WHERE ';
+      whereAdded = true;
+    }
+    queryString += `cost_per_night >= $${queryParams.length}`;
+  }
+  
+  if (options.maximum_price_per_night) {
+    queryParams.push(`${options.maximum_price_per_night}`);
+    if (whereAdded) {
+      queryString += ' AND ';
+    } else {
+      queryString += ' WHERE ';
+      whereAdded = true;
+    }
+    queryString += `cost_per_night <= $${queryParams.length}`;
+  }
+  
+  queryString += ` GROUP BY properties.id`;
+  
+  if (options.minimum_rating) {
+    queryParams.push(`${options.minimum_rating}`);
+    queryString += ` HAVING AVG(property_reviews.rating) >= $${queryParams.length}`;
+  }
+  
+  queryParams.push(limit);
+  queryString += ` ORDER BY cost_per_night LIMIT $${queryParams.length};`;
+  
   return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then((result) => result.rows)
+    .query(queryString, queryParams)
+    .then((result) => {
+      return result.rows;
+    })
     .catch((err) => err.message);
 };
 exports.getAllProperties = getAllProperties;
